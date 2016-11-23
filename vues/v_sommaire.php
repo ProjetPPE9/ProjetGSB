eb85bcd39b797381c14b154f9821f699e07055a8
Alexis <Alexis@PC-Alexis>
2016-11-23 23:51:11 +0100

    Modification de la page sommaire Création des 2 controleurs gererFraisForfait et gererFraisHorsForfait

--- vues/v_sommaire.php
+++ vues/v_sommaire.php
@@ -38,8 +38,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php?uc=gererFrais&action=saisirFrais">Saisir mes fiches de frais forfaitisées</a></li>
-        <li class="active"><a href="index.php?uc=gererFrais&action=saisirFrais">Saisir mes fiches de frais hors forfait</a></li>
+        <li class="active"><a href="index.php?uc=gererFraisForfait&action=saisirFrais">Saisir mes fiches de frais forfaitisées</a></li>
+        <li class="active"><a href="index.php?uc=gererFraisHorsForfait&action=saisirFrais">Saisir mes fiches de frais hors forfait</a></li>
         <li class="active"><a href="index.php?uc=etatFrais&action=selectionnerMois">Mes fiches de frais</a></li>   
       </ul>
       <ul class="nav navbar-nav navbar-right">
